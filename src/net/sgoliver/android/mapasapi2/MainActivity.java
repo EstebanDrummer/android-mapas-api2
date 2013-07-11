23481fc5cced003e95adc6dc1775baaf7e18b3e0
EstebanDrummer <yepesk8r@gmail.com>
2013-07-10 22:00:58 -0500

    Proyecto Final beta1 android
    Proyecto Seminario Android

---
+++ src/net/sgoliver/android/mapasapi2/MainActivity.java
@@ -0,0 +1,132 @@
+package net.sgoliver.android.mapasapi2;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.GoogleMap.OnCameraChangeListener;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import com.google.android.gms.maps.Projection;
+
+import android.graphics.Point;
+
+public class MainActivity extends android.support.v4.app.FragmentActivity {
+
+	private GoogleMap mapa = null;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		inicio();//inicializamos mapa en UdeA
+		mapa.setOnMapClickListener(new OnMapClickListener() {
+			@Override
+			public void onMapClick(LatLng point) {
+				// TODO Auto-generated method stub
+				Projection proj = mapa.getProjection();
+				Point coord = proj.toScreenLocation(point);
+
+				Toast.makeText(
+						MainActivity.this, 
+						"Click\n" + 
+						"Lat: " + point.latitude + "\n" +
+						"Lng: " + point.longitude + "\n" +
+						"X: " + coord.x + " - Y: " + coord.y,
+						Toast.LENGTH_SHORT).show();
+				
+			}
+		});
+		mapa.setOnMapLongClickListener(new OnMapLongClickListener() {
+			public void onMapLongClick(LatLng point) {
+				Projection proj = mapa.getProjection();
+				Point coord = proj.toScreenLocation(point);
+
+				Toast.makeText(
+						MainActivity.this, 
+						"Click Largo\n" + 
+						"Lat: " + point.latitude + "\n" +
+						"Lng: " + point.longitude + "\n" +
+						"X: " + coord.x + " - Y: " + coord.y,
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+		/*mapa.setOnCameraChangeListener(new OnCameraChangeListener() {
+			public void onCameraChange(CameraPosition position) {
+				Toast.makeText(
+						MainActivity.this, 
+						"Cambio Cámara\n" + 
+						"Lat: " + position.target.latitude + "\n" +
+						"Lng: " + position.target.longitude + "\n" +
+						"Zoom: " + position.zoom + "\n" +
+						"Orientación: " + position.bearing + "\n" +
+						"Ángulo: " + position.tilt,
+						Toast.LENGTH_SHORT).show();
+			}
+		});*/
+
+		mapa.setOnMarkerClickListener(new OnMarkerClickListener() {
+			public boolean onMarkerClick(Marker marker) {
+				Toast.makeText(
+						MainActivity.this, 
+						"Marcador pulsado:\n" + 
+						marker.getTitle(),
+						Toast.LENGTH_SHORT).show();
+				return false;
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) 
+	{	
+		switch(item.getItemId())
+		{
+			case R.id.menu_marcadores:
+				mostrarMarcador(6.267221220428, -75.5690885335);
+				break;
+			case R.id.menu_lineas:
+				//mostrarLineas();
+				break;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+	private void inicio(){
+		 
+		mapa = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+		 mapa.setMapType(GoogleMap.MAP_TYPE_HYBRID);//vista hibrida, satelital con nombres
+			LatLng udea = new LatLng(6.267221220428, -75.5690885335);
+			CameraPosition camPos = new CameraPosition.Builder()
+				    .target(udea)   //Centramos el mapa en UdeA
+				    .zoom(17)         //Establecemos el zoom en 17
+				    .build();
+
+			CameraUpdate camUpd3 = 
+					CameraUpdateFactory.newCameraPosition(camPos);
+
+			mapa.animateCamera(camUpd3);
+	}
+	private void mostrarMarcador(double lat, double lng)
+	{
+	    mapa.addMarker(new MarkerOptions()
+	        .position(new LatLng(lat, lng))
+	        .title("Lugar: Biblioteca"));
+	}
+}
